bb0db6842ca8619c45ff0fd0f7baf0732f40d690
Ruben Shrestha <rubstha77@gmail.com>
2022-12-13 16:53:48 +0545

    Add api instance

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -1,8 +1,21 @@
 import axios from 'axios'
 
+
+let headers = {
+    "Content-Type": "application/json",
+  };
+  
+  const baseURL = "https://jsonplaceholder.typicode.com"
+
+  const api = axios.create({
+    baseURL: baseURL,
+    headers: headers
+  });
+  
+
 export async function addToDolist(list){
     try {
-       const  {data} = await axios.post('https://jsonplaceholder.typicode.com/posts',list)
+       const  {data} = await api.post('/posts',list)
         console.log(data)
         return data
     } catch (error) {
@@ -11,7 +24,7 @@ export async function addToDolist(list){
 }
 export async function showToDolist(list){
     try {
-       const  {data} = await axios.get('https://jsonplaceholder.typicode.com/posts')
+       const  {data} = await api.get('/posts')
         return data
     } catch (error) {
        console.log(error) 
--- src/AddToDo.jsx
+++ src/AddToDo.jsx
@@ -7,11 +7,6 @@ export default function AddToDo({task,setTask,dispatch}) {
         <input  type="text" name="todolist" id="todolist" value={task.title} onChange={(e)=>setTask({...task,title:e.target.value})}   />  
         <button onClick={()=>dispatch({type:"ADD_TASK",task:task,setTask:setTask})}>Add to List</button>
     </div>
-    {/* <ul>
-       {todos?.map(({title,isDone},index) => (
-        <li>{title} <button  style={{backgroundColor:isDone==="complete"?"green":""}}>{isDone}</button>  <button onClick={()=>handleDelete(index)}>Delete</button></li>
-       ))}
-    </ul> */}
     </>
   )
 }
